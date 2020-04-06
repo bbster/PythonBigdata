81563b77cc05dafdec7fbccc78c67da6671cdb02
bbster <bbster12@naver.com>
2020-04-06 15:51:03 +0900

    first commit

---
+++ 03-hello.py
@@ -0,0 +1,18 @@
+#print("한줄 주석처리 1 ")
+"""
+print("주석 처리 2")
+여러줄 주석 1
+"""
+'''
+print("주석 처리 3")
+여러줄 주석 2
+'''
+
+print("Hello World!!")
+
+def hello():
+    print("Hello world")
+    print(5+7)
+    print(5,"+", 7, "=", 5+7)
+    
+hello()
---
+++ readme.md
@@ -0,0 +1,2 @@
+### 아나콘다 2020.02 / python 3.7
+ - https://www.anaconda.com/distribution/
